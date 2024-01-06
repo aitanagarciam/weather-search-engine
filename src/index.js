b0b72f24a08577bc1801549e60df7591db788920
aitanagarciam <aitana.gm99@gmail.com>
2024-01-06 10:49:19 -0400

    Implement search function with API
    Using SheCodes weather API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let APIKey = "e30450tcao35053d2a305f02341b3b2d";
+  let APIURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${APIKey}`;
+  axios.get(APIURL).then(refreshWeather);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let h1 = document.querySelector("#city");
   let input = document.querySelector("#search-input");
   h1.innerHTML = input.value;
+  searchCity(input.value);
 }
 
 let searchForm = document.querySelector("#search-form");
